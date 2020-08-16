76058ac5b958aa1232da95b03de4cb62a5872a7f
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-16 07:14:24 +0530

    A Social Media

---
+++ angular/src/app/page-register/page-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-register',
+  templateUrl: './page-register.component.html',
+  styleUrls: ['./page-register.component.css']
+})
+export class PageRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular/src/app/page-searches/page-searches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-searches',
+  templateUrl: './page-searches.component.html',
+  styleUrls: ['./page-searches.component.css']
+})
+export class PageSearchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
