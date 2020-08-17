7d9cfe0cc3afb110eeb92322986b4e43385fb852
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-18 05:02:16 +0530

    A Social Media

--- angular/src/app/page-feed/page-feed.component.ts
+++ angular/src/app/page-feed/page-feed.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../auth.service";
 import { ApiService } from "../api.service";
 
 @Component({
@@ -9,7 +8,7 @@ import { ApiService } from "../api.service";
 })
 export class PageFeedComponent implements OnInit {
 
-    constructor(public auth:AuthService,private api:ApiService) { }
+    constructor(private api:ApiService) { }
 
     ngOnInit() {
         let requestObject = {
