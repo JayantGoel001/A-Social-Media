9eaf880c83b0f22030d27b5a046b7d61213dcec9
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-17 06:51:05 +0530

    A Social Media

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './_app/app.component';
@@ -34,7 +36,10 @@ import { FormBgComponent } from './form-bg/form-bg.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- angular/src/app/page-register/page-register.component.ts
+++ angular/src/app/page-register/page-register.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
-  selector: 'app-page-register',
-  templateUrl: './page-register.component.html',
-  styleUrls: ['./page-register.component.css']
+    selector: 'app-page-register',
+    templateUrl: './page-register.component.html',
+    styleUrls: ['./page-register.component.css']
 })
 export class PageRegisterComponent implements OnInit {
 
-  constructor() { }
+    constructor(private api:ApiService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    public formError = "";
+
+    public credentials = {
+        first_name:'',
+        last_name:'',
+        email:'',
+        password:'',
+        password_confirm:''
+    };
+
+    public formSubmit(){
+        this.formError = "";
+        if (
+            !this.credentials.first_name||
+            !this.credentials.last_name||
+            !this.credentials.email||
+            !this.credentials.password||
+            !this.credentials.password_confirm
+        ) {
+            return this.formError = "All Fields are Required";
+        }
+
+        var re = new RegExp(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/);
+        if (!re.test(this.credentials.email)) {
+            return this.formError = "Please enter a valid email address.";
+        }
+
+        if (this.credentials.password !== this.credentials.password_confirm) {
+            return this.formError="Password dont match.";
+        }
+
+        this.register();
+    }
+
+    private register() {
+        console.log("Register");
+    }
 
 }
