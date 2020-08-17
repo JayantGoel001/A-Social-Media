8500cedadea04b8b8b885822221b97407af19e13
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-18 05:02:16 +0530

    A Social Media

--- angular/src/app/topbar/topbar.component.ts
+++ angular/src/app/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: 'app-topbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth:AuthService) { }
 
   ngOnInit(): void {
   }
