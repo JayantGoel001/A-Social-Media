d63f470677c13ac710e457257427d32c3a44674c
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-19 07:08:25 +0530

    A Social Media

--- angular/src/app/topbar/topbar.component.ts
+++ angular/src/app/topbar/topbar.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
 import { LocalStorageService } from "../local-storage.service";
 import { AlertsService } from "../alerts.service";
+import { UserDataService } from "../user-data.service";
+import { ApiService } from '../api.service';
 
 @Component({
     selector: 'app-topbar',
@@ -12,15 +14,35 @@ import { AlertsService } from "../alerts.service";
 export class TopbarComponent implements OnInit {
 
     constructor(public auth:AuthService,public router:Router,
-                public storage:LocalStorageService,public alert:AlertsService) { }
+                public storage:LocalStorageService,public alert:AlertsService,
+                private centralUserData:UserDataService,private api:ApiService) { }
 
     ngOnInit(): void {
         this.userName = this.storage.getParsedToken().name;
-
+        this.userId = this.storage.getParsedToken()._id;
         this.alert.onAlertEvent.subscribe((msg)=>{
             this.alertMessage = msg;
         });
+        this.centralUserData.getUserData.subscribe((data)=>{
+            this.userData = data;
+            this.numOfFriendsRequests = data.friend_requests.length;
+        });
+        let requestObject = {
+            location:`users/${this.userId}`,
+            type:"GET",
+            authorize:true
+        }
+
+        this.api.makeRequest(requestObject).then((val)=>{
+            console.log(val);
+            this.centralUserData.getUserData.emit(val.user);
+        })
+
     }
+
+    public userData:object = {};
+    public numOfFriendsRequests:number = 0;
+    public userId :String = "";
 
     public query:String = "";
     public userName:String = "";
