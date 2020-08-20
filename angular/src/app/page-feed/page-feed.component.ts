ab93d53e745d18498f6d97af8c15efa2aa03e9f4
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-20 12:44:58 +0530

    A Social Media

--- angular/src/app/page-feed/page-feed.component.ts
+++ angular/src/app/page-feed/page-feed.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../api.service";
 import { Title } from "@angular/platform-browser";
 import { LocalStorageService } from "../local-storage.service";
+import { EventEmitterService } from "../event-emitter.service";
+
 @Component({
     selector: 'app-page-feed',
     templateUrl: './page-feed.component.html',
@@ -12,7 +14,8 @@ export class PageFeedComponent implements OnInit {
     constructor(
         private api:ApiService,
         private title:Title,
-        private storage:LocalStorageService
+        private storage:LocalStorageService,
+        private events:EventEmitterService
     ) { }
 
     ngOnInit() {
@@ -40,5 +43,15 @@ export class PageFeedComponent implements OnInit {
     public changeTheme(newTheme) {
         this.newPostTheme = newTheme;
         this.storage.setPostTheme(newTheme);
+    }
+
+    /**
+     * createPost
+     */
+    public createPost() {
+        if (this.newPostContent.length==0) {
+            return this.events.onAlertEvent.emit("No Content For Your Post was Provided.");
+        }
+        console.log(this.newPostContent);
     }
 }
