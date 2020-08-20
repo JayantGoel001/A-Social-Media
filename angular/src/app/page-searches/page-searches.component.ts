e3c87c9ffb104ea333fcd98ed9897040bdcb8247
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-20 08:43:09 +0530

    A Social Media

--- angular/src/app/page-searches/page-searches.component.ts
+++ angular/src/app/page-searches/page-searches.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Inject} from '@angular/core';
 import { ApiService } from '../api.service';
 import { ActivatedRoute } from "@angular/router";
 import { Title } from "@angular/platform-browser";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
     selector: 'app-page-searches',
@@ -11,9 +12,12 @@ import { Title } from "@angular/platform-browser";
 export class PageSearchesComponent implements OnInit {
 
     constructor(private api:ApiService,
-                private route:ActivatedRoute,private title:Title) { }
+                private route:ActivatedRoute,
+                private title:Title,
+                @Inject(DOCUMENT) private document:Document) { }
     ngOnInit() {
         this.title.setTitle("Search Page");
+        this.document.getElementById("sidebarToggleTop").classList.add("d-none");
         this.subscription = this.route.params.subscribe(params=>{
             this.query = params.query;
             this.getResults();
