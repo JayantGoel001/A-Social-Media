7f7808371a00a50e57990c6df4ff611398980f73
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-22 09:22:31 +0530

    A Social Media

--- angular/src/app/page-profile/page-profile.component.ts
+++ angular/src/app/page-profile/page-profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Title } from "@angular/platform-browser";
+import { ActivatedRoute } from "@angular/router";
 import { DOCUMENT } from "@angular/common";
-
+import { UserDataService } from "../user-data.service";
+import { ApiService } from "../api.service";
 
 @Component({
     selector: 'app-page-profile',
@@ -10,11 +12,65 @@ import { DOCUMENT } from "@angular/common";
 })
 export class PageProfileComponent implements OnInit {
 
-    constructor(private title:Title,@Inject(DOCUMENT) private document:Document) { }
+    constructor(
+        private title:Title,
+        @Inject(DOCUMENT) private document:Document,
+        private route:ActivatedRoute,
+        private centralUserData:UserDataService,
+        private api:ApiService) { }
 
     ngOnInit(): void {
         this.title.setTitle("Your Profile");
         this.document.getElementById("sidebarToggleTop").classList.add("d-none");
+
+        let paramsId = this.route.snapshot.params.userid;
+        this.centralUserData.getUserData.subscribe((user)=>{
+            if (user._id==paramsId) {
+                this.setComponentValues(user);
+            }
+            else{
+
+            }
+        })
+
+    }
+
+    public randomFriends:String[] = [];
+    public totalFriends:Number = 0;
+    public posts:Object[] = [];
+    public showPosts:number = 6;
+    public profilePicture:String = "default_avatar";
+    public userName:String = "";
+    public userEmail:String = "";
+
+    public canAddUser:Boolean = false;
+    public canSendMessage:Boolean = false;
+
+    /**
+     * showMorePosts
+     */
+    public showMorePosts() {
+        this.showPosts+=6;
+    }
+
+    /**
+     * name
+     */
+    public backToTop() {
+        this.document.body.scrollTop = this.document.documentElement.scrollTop=0;
+    }
+
+    /**
+     * setComponentValues
+     */
+    public setComponentValues(user) {
+        this.randomFriends = user.random_friend;
+        this.profilePicture = user.profile_image;
+        this.posts = user.posts;
+        this.userName = user.name;
+        this.userEmail = user.email;
+        this.totalFriends = user.friends.length;
+
     }
 
 }
