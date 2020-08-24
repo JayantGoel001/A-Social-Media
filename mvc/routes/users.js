91c7988bedf7749b78feddeba5b9d279d715bdd7
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-24 14:47:20 +0530

    A Social Media

--- mvc/routes/users.js
+++ mvc/routes/users.js
@@ -6,37 +6,40 @@ const userCtrl = require('../controllers/users');
 const fakeUserCtrl = require('../controllers/fake-users');
 
 router.post("/register",userCtrl.registerUser);
-
 router.post("/login",userCtrl.loginUser);
 
-router.get('/generate-feed',middleware.authorize,userCtrl.generateFeed);
 
+router.get('/generate-feed',middleware.authorize,userCtrl.generateFeed);
+router.get('/get-user-data/:userid',middleware.authorize,userCtrl.getUserData);
 router.get('/get-search-results',middleware.authorize,
             userCtrl.getSearchResults);
 
-router.post('/make-friend-request/:from/:to',middleware.authorize,
-            userCtrl.makeFriendRequest);
 
 router.get('/get-friend-requests',middleware.authorize,
             userCtrl.getFriendRequest);
-
-router.get('/get-user-data/:userid',middleware.authorize,userCtrl.getUserData);
-
+router.post('/make-friend-request/:from/:to',middleware.authorize,
+            userCtrl.makeFriendRequest);
 router.post('/resolve-friend-request/:from/:to',middleware.authorize,
             userCtrl.resolveFriendRequest);
+
+
 router.post('/create-post',middleware.authorize,userCtrl.createPost);
 router.post('/like-Unlike/:ownerid/:postid',middleware.authorize,
             userCtrl.likeUnlike);
 router.post('/post-comment/:ownerid/:postid',middleware.authorize,
             userCtrl.postCommentOnPost);
+
+
 router.post('/send-message/:to',middleware.authorize,userCtrl.sendMessage);
 router.post('/reset-message-notifications',middleware.authorize,
             userCtrl.resetMessageNotification);
 router.post('/delete-message/:messageid',middleware.authorize,
             userCtrl.deleteMessage);
 
+
 router.delete('/all',userCtrl.deleteAllUsers);
 router.get('/all',userCtrl.getAllUsers);
+
 router.post("/create-fake-users",fakeUserCtrl.createFakeUsers);
 
 
