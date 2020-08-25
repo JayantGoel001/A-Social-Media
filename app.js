6e8c298ffe2a95f387825925c8c79bc3ce3e47f6
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-25 13:33:35 +0530

    A Social Media

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname,'angular' ,'build')));
+// app.use(express.static(path.join(__dirname,'angular' ,'build')));
 app.use(function(req,res,next) {
     res.statusJson = function(statusCode,data) {
         let obj = {
@@ -42,9 +42,9 @@ app.use('/',(req,res,next)=>{
 })
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.get('*',function(req,res,next) {
-    res.sendFile(path.join(__dirname,'angular','build','index.html'));
-})
+// app.get('*',function(req,res,next) {
+//     res.sendFile(path.join(__dirname,'angular','build','index.html'));
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
