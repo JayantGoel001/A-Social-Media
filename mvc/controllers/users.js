1f002e6a7e1f72708290288c7f2d5b7029c4dea9
JayantGoel001 <jgoel92@gmail.com>
2021-07-13 07:55:03 +0530

    Initial Commit

---
+++ mvc/controllers/users.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+
+const registerUser = ({body},res) =>{
+    if (!Object.values(body).every((val) => val )) {
+        return res.send({message:"All Fields Are Required"});
+    }
+    if (body.password!==body.confirmPassword) {
+        return res.send({message:"Password Does not match"});
+    }
+
+    const user = new User();
+    user.firstName = body.firstName.trim();
+    user.lastName = body.lastName.trim();
+    user.email = body.email.trim();
+    user.setPassword(body.password);
+
+    user.save().then(r =>{
+        res.status(201).json({message: "New User", user: r});
+    }).catch((err)=>{
+        res.status(400).json({message: err});
+    });
+}
+
+const loginUser = (req,res) =>{
+    let body = req.body;
+    if (!body.email || !body.password) {
+        return res.status(400).json({message:"All Fields are Required."});
+    }
+    passport.authenticate("local",(err,user,info)=>{
+        if (err) {
+            return res.status(400).json({message:err});
+        }
+        if (user) {
+            res.status(201).json({message:"Logged In"});
+        }  else {
+            res.status(401).json(info);
+        }
+    })(req,res);
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+}
--- mvc/routes/users.js
+++ mvc/routes/users.js
@@ -1,46 +1,10 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const passport = require('passport');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
+const userCtrl = require("../controllers/users");
 
-router.post("/register",function({body},res) {
-    if (!Object.values(body).every((val) => val )) {
-        return res.send({message:"All Fields Are Required"});
-    }
-    if (body.password!==body.confirmPassword) {
-        return res.send({message:"Password Doesnot match"});
-    }
+router.post("/register",userCtrl.registerUser) ;
 
-    const user = new User();
-    user.firstName = body.firstName.trim();
-    user.lastName = body.lastName.trim();
-    user.email = body.email.trim();
-    user.setPassword(body.password);
-
-    user.save().then(r =>{
-        res.status(201).json({message: "New User", user: r});
-    }).catch((err)=>{
-        res.status(400).json({message: err});
-    });
-}) ;
-
-router.post("/login",function(req,res) {
-    let body = req.body;
-    if (!body.email || !body.password) {
-        return res.status(400).json({message:"All Fields are Required."});
-    }
-    passport.authenticate("local",(err,user,info)=>{
-        if (err) {
-            return res.status(400).json({message:err});
-        }
-        if (user) {
-            res.status(201).json({message:"Logged In"});
-        }  else {
-            res.status(401).json(info);
-        }
-    })(req,res);
-});
+router.post("/login",userCtrl.loginUser);
 
 module.exports = router;
