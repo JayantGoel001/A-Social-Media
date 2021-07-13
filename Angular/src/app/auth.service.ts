f457e999ec6fab917c9eaab44a852b624bbdff67
JayantGoel001 <jgoel92@gmail.com>
2021-07-13 15:14:00 +0530

    Initial Commit

---
+++ Angular/src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class AuthService {
+
+	constructor(private router : Router) {
+
+	}
+	public canActivate(route :ActivatedRouteSnapshot, state : RouterStateSnapshot ) : boolean{
+		let activate = this.isLoggedIn();
+		let redirect = "/feed";
+		if (route.data && route.data.loggedIn){
+			activate = !activate;
+			redirect = "/register";
+		}
+		if (!activate){
+			return true;
+		}else {
+			this.router.navigate([redirect]).then(r => {  });
+			return false;
+		}
+	}
+	public isLoggedIn(){
+		return true;
+	}
+}
