b5dc8d88ddbd45027d763cc21f737342a6b87a8b
JayantGoel001 <jgoel92@gmail.com>
2021-07-14 21:40:26 +0530

    Initial Commit

--- Angular/src/app/sidebar/sidebar.component.ts
+++ Angular/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth:AuthService) { }
 
   ngOnInit(): void {
   }
--- Angular/src/app/topbar/topbar.component.ts
+++ Angular/src/app/topbar/topbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
 
 @Component({
-  selector: 'app-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+	selector: 'app-topbar',
+	templateUrl: './topbar.component.html',
+	styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+	constructor(public auth: AuthService) {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
