3131e79ae42b1d25bf91a4334366ec4d01728d7b
JayantGoel001 <jgoel92@gmail.com>
2021-07-16 02:54:56 +0530

    Initial Commit

--- Angular/src/app/page-friend-requests/page-friend-requests.component.ts
+++ Angular/src/app/page-friend-requests/page-friend-requests.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {UserDataService} from "../user-data.service";
 
 @Component({
-  selector: 'app-page-friend-requests',
-  templateUrl: './page-friend-requests.component.html',
-  styleUrls: ['./page-friend-requests.component.css']
+	selector: 'app-page-friend-requests',
+	templateUrl: './page-friend-requests.component.html',
+	styleUrls: ['./page-friend-requests.component.css']
 })
 export class PageFriendRequestsComponent implements OnInit {
+	public data:any
 
-  constructor() { }
+	constructor(private userData :UserDataService) {  }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.userData.getUserData.subscribe((data)=>{
+			this.data = data;
+			console.log(this.data);
+		});
+	}
+
 
 }
