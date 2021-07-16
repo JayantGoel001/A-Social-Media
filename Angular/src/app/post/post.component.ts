ce75a71e478f4373a179867845b79b8c50e5cf92
JayantGoel001 <jgoel92@gmail.com>
2021-07-17 01:19:04 +0530

    Initial Commit

--- Angular/src/app/post/post.component.ts
+++ Angular/src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+	@Input() post: any;
 
-  ngOnInit(): void {
-  }
+	public fakeID : string = "fakeID";
 
+	constructor() {  }
+
+	ngOnInit(): void {
+		let removeLeadingNumber = (id:string)=>{
+			let isNumber = (n:any)=>{
+				n = Number(n);
+				return !isNaN(n);
+			}
+			if (id && isNumber(id[0])){
+				id = removeLeadingNumber(id.substr(1));
+			}
+			return id;
+		}
+		this.fakeID = removeLeadingNumber(this.post._id);
+	}
 }
--- mvc/routes/users.js
+++ mvc/routes/users.js
@@ -10,12 +10,10 @@ router.post("/login",userCtrl.loginUser);
 router.get("/generate-feed",middleware.authorize,userCtrl.generateFeed);
 router.post("/create-post",middleware.authorize,userCtrl.createPost);
 
-
 router.get("/search-results",middleware.authorize,userCtrl.getSearchResult);
 router.post("/send-friend-request/:from/:to",middleware.authorize,userCtrl.sendFriendRequest);
 router.get("/get-friend-requests",middleware.authorize,userCtrl.getFriendsRequests);
 router.post("/resolve-friend-request/:from/:to",middleware.authorize,userCtrl.resolveFriendRequest);
-
 
 router.delete("/all",userCtrl.deleteAllUsers);
 router.get("/all",userCtrl.getAllUsers);
