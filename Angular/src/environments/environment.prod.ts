ba2324ffb68ade46ce40ff0b111f39807a2c6f7a
JayantGoel001 <jgoel92@gmail.com>
2021-07-18 22:15:16 +0530

    Initial Commit

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	baseUrl : "https://github.com/JayantGoel001/A-Social-Media",
+	baseUrl : "https://github.com/JayantGoel001/A-Social-Media/",
 	production: true
 };
--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	baseUrl : "http://localhost:3000",
+	baseUrl : "http://localhost:3000/",
 	production: false
 };
 
