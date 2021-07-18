54517522fd8d5de69ceed22ff3e06b545650d054
JayantGoel001 <jgoel92@gmail.com>
2021-07-18 22:07:35 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(function(req,res,next) {
 app.use(passport.initialize());
 
 app.use('/',(req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','http://localhost:4200');
+    res.header('Access-Control-Allow-Origin','https://jayantgoel001.github.io/A-Social-Media/');
     res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
     next();
 })
