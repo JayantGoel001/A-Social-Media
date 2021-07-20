160ba18631a18290b4fc6e7a54dc213856ad98a1
JayantGoel001 <jgoel92@gmail.com>
2021-07-21 00:28:37 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(function(req,res,next) {
 app.use(passport.initialize());
 
 app.use('/',(req,res,next)=>{
-
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
