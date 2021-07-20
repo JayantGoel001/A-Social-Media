a6bdc58e0875f4437cf3dbe781ac506949b7eb15
JayantGoel001 <jgoel92@gmail.com>
2021-07-21 00:30:19 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -49,18 +49,15 @@ app.use('/users', usersRouter);
 app.get("*",function (req, res) {
     res.redirect("https://jayantgoel001.github.io/A-Social-Media/");
 })
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
     next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
