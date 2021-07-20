32f59303b94f0f233adb7b153b287c4f56a210c7
JayantGoel001 <jgoel92@gmail.com>
2021-07-21 00:33:53 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -36,9 +36,11 @@ app.use(function(req,res,next) {
 app.use(passport.initialize());
 
 app.use('/',(req,res,next)=>{
-    let url = "http://localhost:4200/";
+    let url = "*";
     if(process.env.NODE_ENV==="PRODUCTION"){
         url = "https://jayantgoel001.github.io";
+    }else {
+        url = "http://localhost:4200/";
     }
     res.header('Access-Control-Allow-Origin',url);
     res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
