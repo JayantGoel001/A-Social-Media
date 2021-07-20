c28a8d30d8566d2bb55193bc2a67681c1be70907
JayantGoel001 <jgoel92@gmail.com>
2021-07-21 00:59:29 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname,'Angular', 'build')));
 
 app.use(function(req,res,next) {
     res.statusJson = function(statusCode,data) {
